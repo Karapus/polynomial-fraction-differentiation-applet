8757b61d41bb5c106fd4944c2ae6de322febdc96
Karapus <evgeny.nay@outlook.com>
2023-05-26 20:15:36 +0300

    lexer

--- HelloWorldApplet.java
+++
@@ -1,8 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-
-public class HelloWorldApplet extends Applet {
-   public void paint (Graphics g) {
-      g.drawString ("Привет, Мир", 25, 50);
-   }
-}
